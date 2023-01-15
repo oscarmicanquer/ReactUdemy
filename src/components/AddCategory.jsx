92a77b262eed02a450d839b1ce0d92af3821109a
Oscar <oscarmicanquer@gmail.com>
2023-01-15 12:35:39 -0500

    error en vista

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+
+export const AddCcategory = ({onNewCategory}) => {
+   
+   const [inputValue, setInputValue] = useState('One Punch')
+
+   const onInputChange = ({target}) =>{
+        setInputValue(target.value)
+   }
+
+   const onSubmit = (event) => {
+    event.preventDefault()
+    if (inputValue.trim().length<=1)return;
+    
+    setInputValue('');
+    onNewCategory(inputValue.trim());
+    
+   }
+   
+    return (
+        <form onSubmit={ onSubmit}>
+            <input 
+                type="text"
+                placeholder="Buscar Gifs"
+                value={inputValue}
+                onChange={onInputChange}
+            />
+        </form>
+
+        
+    )
+}
